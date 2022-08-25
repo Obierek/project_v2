e8ec0a6844594f8ea4483474adaac6121018ec46
Obierek <marcin.obierzynski@gmail.com>
2022-08-25 17:00:05 +0200

    add difficulty

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import ChangeDifficulty from "./ChangeDifficulty";
 
-const api = "http://localhost:3000/questions_medium"
+const api = "http://localhost:4000/questions_medium"
 
 export default function App() {
 
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [showScore, setShowScore] = useState(false);
     const [score, setScore] = useState(0);
-    const [questions, setQuestions] = useState("");
+    const [questions, setQuestions] = useState([]);
+    const [questionsForLevel, setQuestionsForLevel] = useState([]);
+    const [level, setLevel] = useState("easy")
 
     useEffect(() => {
-        fetch(api)
+        setQuestionsForLevel(questions.filter(item => {
+            return item.level === level
+        }))
+    }, [level])
+
+
+    useEffect(async () => {
+
+        await fetch(api)
             .then((res) => {
                 if (res.ok) {
                     return res.json();
                 }
-                throw new Error("Oops...");
+
             })
             .then(data => {
                 setQuestions(data);
@@ -54,29 +64,60 @@ export default function App() {
         }
 
     };
+    console.log(questions);
+
     return (
-            <div className='app'>
-                <h1>Quiz</h1>
-                {showScore ? (
-                    <div className='score'>
-                        Odpowiedziałeś poprawnie na {score} z {questions.length} pytań!
+        <div className='app'>
+            <h1>Quiz</h1>
+            {showScore ?
+                <div className='score'>
+                    Odpowiedziałeś poprawnie na {score} z {questionsForLevel.length} pytań!
+                </div>
+                :
+                <>
+                    <div className='question'>
+                        <div className='question__count'>
+                            <span>Pytanie {currentQuestion + 1} z {questionsForLevel.length}</span>
+                        </div>
+                        <div
+                            className='question__text'>{questionsForLevel[currentQuestion]?.questionText ?? "Question cannot be loaded"}</div>
                     </div>
-                ) : (
-                    <>
-                        <div className='question'>
-                            <div className='question__count'>
-                                <span>Pytanie {currentQuestion + 1} z {questions.length}</span>
-                            </div>
-                            <div className='question__text'>{questions[currentQuestion].questionText}</div>
-                        </div>
-                        <div className='answer'>
-                            {questions[currentQuestion].answerOptions.map((answerOption) => (
-                                <button
-                                    onClick={() => handleClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-                            ))}
-                        </div>
-                    </>
-                )}
-            </div>
+                    {questionsForLevel[currentQuestion]?.answerOptions && <div className='answer'>
+                        {questionsForLevel[currentQuestion].answerOptions.map((answerOption) => (
+                            <button
+                                onClick={() => handleClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                        ))}
+                    </div>}
+                </>
+            }
+        </div>
     );
-}
+};
+
+
+//     return (
+//             <div className='app'>
+//                 <h1>Quiz</h1>
+//                 {showScore ? (
+//                     <div className='score'>
+//                         Odpowiedziałeś poprawnie na {score} z {questions.length} pytań!
+//                     </div>
+//                 ) : (
+//                     <>
+//                         <div className='question'>
+//                             <div className='question__count'>
+//                                 <span>Pytanie {currentQuestion + 1} z {questions.length}</span>
+//                             </div>
+//                             <div className='question__text'>{questions[currentQuestion].questionText}</div>
+//                         </div>
+//                         <div className='answer'>
+//                             {questions[currentQuestion].answerOptions.map((answerOption) => (
+//                                 <button
+//                                     onClick={() => handleClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+//                             ))}
+//                         </div>
+//                     </>
+//                 )}
+//             </div>
+//     );
+// }
