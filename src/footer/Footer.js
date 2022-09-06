c85db28e2a66b6f28674000bda5799e74cbe2734
Obierek <marcin.obierzynski@gmail.com>
2022-09-06 13:05:10 +0200

    add style and footer

---
+++ src/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div className="container">
+            <div className="footer">
+                 <p className="footer__link" href="#">Copyright ©</p>
+
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import NavMenu from "./navMenu/NavMenu";
 import App from "./app/App";
+import Footer from "./footer/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <NavMenu/>
         <App/>
+        <Footer/>
     </React.StrictMode>
 );
 
