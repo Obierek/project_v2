fcdb1ce9b80c9df294dbe9e396bb13a456973394
Obierek <marcin.obierzynski@gmail.com>
2022-10-26 10:41:28 +0200

    update

--- src/navMenu/NavMenu.js
+++ src/navMenu/NavMenu.js
@@ -6,7 +6,7 @@ export default function NavMenu() {
     return (
         <div className="container">
             <div className="nav">
-                <Link to="/" className="nav__link" >Home</Link>
+                <Link to="/project_quiz" className="nav__link" >Home</Link>
                 <Link to="app" className="nav__link" href="#">App</Link>
                 <Link to="github" className="nav__link" href="#">GitHub</Link>
                 <Link to="about" className="nav__link" href="#">About me</Link>
